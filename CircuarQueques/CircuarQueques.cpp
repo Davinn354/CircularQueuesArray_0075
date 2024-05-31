318c0c342679323af310d8352a73f99cbbd1a737
Davinn354 <daviipad53@gmail.com>
2024-05-31 14:56:17 +0700

    Penambahan if while di void display

--- CircuarQueques/CircuarQueques.cpp
+++ CircuarQueques/CircuarQueques.cpp
@@ -73,5 +73,13 @@ public:
 
 		cout << "\nElement in the queue are...\n";
 
+		//Jika FRONT_position <= REAR_position, yterasi dari FRONT hingga REAR
+		if (FRONT_position <= REAR_position) {
+			while (FRONT_position <= REAR_position) {
+				cout << queue_array[FRONT_position] << "    ";
+				FRONT_position++;
+			}
+		}
+
 	}
 };
